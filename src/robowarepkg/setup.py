6e00cdec0021cf2fcc67a621b343fb2cca5b334a
Altairu <106.nogu@gmail.com>
2025-03-24 15:48:07 +0900

    new

---
+++ src/robowarepkg/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'robowarepkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='altair',
+    maintainer_email='106.nogu@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'roboware_node = robowarepkg.roboware_node:main'
+        ],
+    },
+)
